d6957cbab9c642e7e3a0a289db0069a28baf0601
m4rquee <lucasxm12@gmail.com>
2021-11-19 19:32:35 -0300

    CameraControl refac

--- Assets/Scripts/CameraControl/CameraController.cs
+++ Assets/Scripts/CameraControl/CameraController.cs
@@ -4,45 +4,56 @@ namespace CameraControl
 {
     public class CameraController : MonoBehaviour
     {
-        public float panSpeed = 20f;
-        public float xMin, xMax;
-        public float zMin, zMax;
+        [Tooltip("The speed at which the camera will move in the xz plane.")]
+        [Range(float.Epsilon, 100f)]
+        [SerializeField]
+        private float panSpeed = 20f;
 
-        public float scrollSpeed = 2000f;
-        public float yMin, yMax;
+        [Tooltip("The speed at which the camera will move in the y direction.")]
+        [Range(float.Epsilon, 100000f)]
+        [SerializeField]
+        private float scrollSpeed = 2000f;
 
-        public float rotationSpeed = 20f;
-        public float rotMin, rotMax;
+        [Tooltip("Minimum/maximum values imposed in the y coordinate.")] [SerializeField]
+        private float yMin, yMax;
+
+        [Tooltip("The speed at which the camera will rotate around its y axis.")]
+        [Range(float.Epsilon, 1000f)]
+        [SerializeField]
+        private float rotationSpeed = 80f;
 
         private void Update()
         {
-            var pos = transform.position;
-            if (Input.GetKey("w"))
-                pos.z += panSpeed * Time.deltaTime;
+            var trans = transform;
+            var pos = trans.position;
+
+            var forward = trans.forward;
+            forward.y = 0;
             if (Input.GetKey("s"))
-                pos.z -= panSpeed * Time.deltaTime;
+                forward *= -1;
+            else if (!Input.GetKey("w"))
+                forward *= 0;
+
+            var right = trans.right;
+            right.y = 0;
             if (Input.GetKey("a"))
-                pos.x -= panSpeed * Time.deltaTime;
-            if (Input.GetKey("d"))
-                pos.x += panSpeed * Time.deltaTime;
+                right *= -1;
+            else if (!Input.GetKey("d"))
+                right *= 0;
 
             var scroll = Input.GetAxis("Mouse ScrollWheel");
+
+            pos += Time.deltaTime * panSpeed * (forward + right);
             pos.y += scroll * scrollSpeed * Time.deltaTime;
-
-            pos.x = Mathf.Clamp(pos.x, xMin, xMax);
             pos.y = Mathf.Clamp(pos.y, yMin, yMax);
-            pos.z = Mathf.Clamp(pos.z, zMin, zMax);
-            pos.z = Mathf.Clamp(pos.z, zMin, zMax);
-            transform.position = pos;
+            trans.position = pos;
 
-            var rot = transform.rotation.eulerAngles;
+            var rot = Vector3.zero;
             if (Input.GetKey("q"))
-                rot.y -= rotationSpeed * Time.deltaTime;
+                rot.y = -Time.deltaTime * rotationSpeed;
             if (Input.GetKey("e"))
-                rot.y += rotationSpeed * Time.deltaTime;
-
-            rot.y = Mathf.Clamp(rot.y, rotMin, rotMax);
-            transform.rotation = Quaternion.Euler(rot);
+                rot.y = Time.deltaTime * rotationSpeed;
+            trans.Rotate(rot, Space.World);
         }
     }
 }
